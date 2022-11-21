6b3c177ce38ae4e2593fb908f8e6bba78e9d12ea
ahmed <u31999@gmail.com>
2022-11-21 18:08:09 +0200

    Fix image dir

--- js/data.js
+++ js/data.js
@@ -4,35 +4,35 @@ export const allMenu = [
         menu : [
             {
                 name: 'burratta pizza',
-                src: '/res/menu/menu-desh/Arugula and burratta pizza.jpg',
+                src: '../res/menu/menu-desh/Arugula and burratta pizza.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
             {
                 name: 'crunchy black',
-                src: '/res/menu/menu-desh/crunchy black.jpg',
+                src: '../res/menu/menu-desh/crunchy black.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'Jimmy Willy Pizza',
-                src: '/res/menu/menu-desh/Jimmy Willy Pizza.jpg',
+                src: '../res/menu/menu-desh/Jimmy Willy Pizza.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
             {
                 name: 'Pepperoni Pizza',
-                src: '/res/menu/menu-desh/Pepperoni and Olive Pizza.jpg',
+                src: '../res/menu/menu-desh/Pepperoni and Olive Pizza.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'herbs mushrooms pizza',
-                src: '/res/menu/menu-desh/pizza with herbs mushrooms.jpg',
+                src: '../res/menu/menu-desh/pizza with herbs mushrooms.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'hotdog pizza',
-                src: '/res/menu/menu-desh/hotdogpizza.jpg',
+                src: '../res/menu/menu-desh/hotdogpizza.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
@@ -43,25 +43,25 @@ export const allMenu = [
         menu: [
             {
                 name: 'best burger',
-                src: '/res/menu/menu-desh/best burger.jpg',
+                src: '../res/menu/menu-desh/best burger.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'cheese burger',
-                src: '/res/menu/menu-desh/cheese-burger.jpg',
+                src: '../res/menu/menu-desh/cheese-burger.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'burger meal',
-                src: '/res/menu/menu-desh/burger-meal.jpg',
+                src: '../res/menu/menu-desh/burger-meal.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             },
             {
                 name: 'souce burger',
-                src: '/res/menu/menu-desh/souce-burger.jpg',
+                src: '../res/menu/menu-desh/souce-burger.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
 
             }
@@ -72,32 +72,32 @@ export const allMenu = [
         menu: [
              {
                 name: 'Pizza Ice-cream',
-                src: '/res/menu/menu-desh/Pizza Icecream.jpg',
+                src: '../res/menu/menu-desh/Pizza Icecream.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
              {
                 name: 'coffe ice-cream',
-                src: '/res/menu/menu-desh/coffe-icecream.jpg',
+                src: '../res/menu/menu-desh/coffe-icecream.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
              {
                 name: 'Ice-cream cake',
-                src: '/res/menu/menu-desh/Icecream cake.jpg',
+                src: '../res/menu/menu-desh/Icecream cake.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
              {
                 name: 'Oreo Frappuccino',
-                src: '/res/menu/menu-desh/Oreo Frappuccino.jpg',
+                src: '../res/menu/menu-desh/Oreo Frappuccino.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
              {
                 name: 'Strawberries Ice-Cream',
-                src: '/res/menu/menu-desh/Strawberries-Ice Cream.jpg',
+                src: '../res/menu/menu-desh/Strawberries-Ice Cream.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
              {
                 name: 'ice-cream cup',
-                src: '/res/menu/menu-desh/ice-cream-cup.jpg',
+                src: '../res/menu/menu-desh/ice-cream-cup.jpg',
                 txt: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
             },
         ]
